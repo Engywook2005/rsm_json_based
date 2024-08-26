78c8683a0ab6e71958f48e66a76bc9d9f3e7ca54
Greg Thorson <Engywook2005@gmail.com>
2024-08-26 18:06:49 -0400

    Slight update to settings.json

--- app/main.js
+++ app/main.js
@@ -5,9 +5,6 @@ import { buildSidebar } from './parser';
 window.parseResume = async function (jsonPath, target) {
   const rsmObj = await fetch(jsonPath);
   console.log(rsmObj);
-  if (fish == 'poo') {
-    console.log('fish poo');
-  }
   if (rsmObj.rsm) {
     const sidebar = buildElement('sidebar');
     buildSidebar(rsmObj.rsm, sidebar);
